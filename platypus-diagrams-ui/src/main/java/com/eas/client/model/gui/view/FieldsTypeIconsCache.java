a6ef0cf4a59ca8abd0ccbff789e72c04858520d6
Марат Гайнуллин <mgainullin@ivanovo.indusoft.ru>
2017-02-13 14:16:56 +0300

    Icons of datatypes refactored

--- platypus-diagrams-ui/src/main/java/com/eas/client/model/gui/view/FieldsTypeIconsCache.java
+++ platypus-diagrams-ui/src/main/java/com/eas/client/model/gui/view/FieldsTypeIconsCache.java
@@ -1,12 +1,3 @@
-/* Datamodel license.
- * Exclusive rights on this code in any form
- * are belong to it's author. This code was
- * developed for commercial purposes only. 
- * For any questions and any actions with this
- * code in any form you have to contact to it's
- * author.
- * All rights reserved.
- */
 package com.eas.client.model.gui.view;
 
 import com.eas.client.model.gui.IconCache;
@@ -28,7 +19,7 @@ public class FieldsTypeIconsCache {
     static {
         icons16.put(null, "datatypes/16x16/blob.png");
         icons16.put(Scripts.NUMBER_TYPE_NAME, "datatypes/16x16/number.png");
-        icons16.put(Scripts.BOOLEAN_TYPE_NAME, "datatypes/16x16/flag.png");
+        icons16.put(Scripts.BOOLEAN_TYPE_NAME, "datatypes/16x16/boolean.png");
         icons16.put(Scripts.STRING_TYPE_NAME, "datatypes/16x16/text.png");
         icons16.put(Scripts.DATE_TYPE_NAME, "datatypes/16x16/date.png");
         icons16.put(Scripts.GEOMETRY_TYPE_NAME, "datatypes/16x16/geometry.png");
