27915e4469a8f5b12fccfd1f90de2881bb5d6179
Марат Гайнуллин <mgainullin@ivanovo.indusoft.ru>
2017-02-28 18:03:27 +0300

    Preparations to publishing

---
+++ platypus-js-samples/src/main/java/com/eas/designer/samples/PlatypusSamples.java
@@ -0,0 +1,10 @@
+package com.eas.designer.samples;
+
+/**
+ *
+ * @author mg
+ */
+public class PlatypusSamples {
+    public static final String NAME = "name"; //NOI18N
+    public static final String LOCATION_DIR = "projdir"; //NOI18N
+}
